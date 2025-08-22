0b79b6daa3a8adaf57250bfb78fe23b39518b224
FerGM1305 <fguzmanm1305@outlook.com>
2025-08-22 13:29:50 -0600

    Controller correction

--- src/main/java/com/movies/directors/controller/DirectorsController.java
+++ src/main/java/com/movies/directors/controller/DirectorsController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.validation.constraints.Min;
 import java.util.List;
 
+import java.util.Map;
+import java.util.HashMap;
+
 /**
  * REST controller that exposes endpoints related to directors.
  * This class acts as the entry point for HTTP requests from clients.
@@ -30,13 +33,19 @@ public class DirectorsController {
      /**
       * GET endpoint: /api/directors
       *
-      * Returns a list of directors who have directed more than "threshold" movies.
+      * Returns a JSON object containing a list of directors who have directed more than "threshold" movies.
       * Only movies from 2010 onwards are considered.
       * @param threshold The minimum number of movies a director must have directed (must be >= 0).
-      * @return A ResponseEntity containing the list of directors.
+      * @return A ResponseEntity containing a Map with key "directors" and the list of directors as value
       */
     @GetMapping("/directors")
-    public ResponseEntity<List<String>> getDirectors(@RequestParam @Min(0) int threshold) {
-        return ResponseEntity.ok(directorService.getDirectorsAboveThreshold(threshold));
+    public ResponseEntity<Map<String, List<String>>>  getDirectors(@RequestParam @Min(0) int threshold) {
+        //return ResponseEntity.ok(directorService.getDirectorsAboveThreshold(threshold));
+    	List<String> directors = directorService.getDirectorsAboveThreshold(threshold);
+
+        Map<String, List<String>> response = new HashMap<>();
+        response.put("directors", directors);
+
+        return ResponseEntity.ok(response);
     }
 }
